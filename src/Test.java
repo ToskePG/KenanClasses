e7e2585fa61f488ce89394cee6c408b5e000f099
ToskePG <danilo.tosic.pmf@gmail.com>
2024-04-23 14:09:57 +0200

    Prva if petlja

---
+++ src/Test.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		// Najveci od tri broja, sa ponavljanjem
+		System.out.println("Unesi prvi cijeli broj");
+		int a = input.nextInt();
+		System.out.println("Unesi drugi cijeli broj");
+		int b = input.nextInt();
+		System.out.println("Unesi treci cijeli broj");
+		int c = input.nextInt();
+		
+		/*if(a>b) {
+			if(a>c) {
+				System.out.println("Najveci broj je " +a);
+			}else {
+				System.out.println("Najveci broj je " +c);
+			}
+		}else {
+			if(b>c) {
+				System.out.println("Najveci broj je " +b);
+			}else {
+				System.out.println("Najveci broj je " +c);
+			}
+		}*/
+		
+		// Najveci od tri broja, bez ponavljanja
+		if(a>b) {
+			if(a>c) {
+				System.out.println("Najveci broj je " +a);
+			}else if(a == c) {
+				System.out.println("Brojevi a i c imaju najvecu vrijednost: " +a);
+			}else {
+				System.out.println("Najveic broj je: " +c);
+			}
+		}else if(a == b) {
+			if(a>c) {
+				System.out.println("Brojevi a, b imaju najvecu vrijednost: " +a);
+			}else if(a == c) {
+				System.out.println("Brojevi a, b, c imaju vrijednost: " +a);
+			}else {
+				System.out.println("Najveic broj je: " +c);
+			}
+		}else {
+			if(b>c) {
+				System.out.println("Najveci broj je: " +b);
+			}else if(b == c) {
+				System.out.println("Brojevi b, c imaju istu vrijednost: " +b);
+			}else {
+				System.out.println("Broj c ima najvecu vrijednost: " +c);
+			}
+		}
+				
+	}
+
+}
